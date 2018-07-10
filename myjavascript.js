1181b945d68f9dd52389e5f37989b9b70d94c33f
pamelarivera <41088805+pamelarivera@users.noreply.github.com>
2018-07-10 15:15:05 -0700

    Create myjavascript.js

---
+++ myjavascript.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded",
+function(){
+//all of your code goes here
+
+
+    // alert("Hello World!");
+    console.log("JEEUUU");
+
+
+    let myPhoto =
+    document.getElementByID("");
+
+
+
+
+})//end of DOM content loaded
